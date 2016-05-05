ccfa1d1cf3b2adb3cad294bbb7847fdc6dc870d5
Jakub Jarabica <jakub@blueweb.sk>
2016-05-05 10:29:20 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,26 @@
+=blueweb/newrelic-nette
+==========================
+
+Most of code is based on [article](http://filip-prochazka.com/blog/newrelic-monitoring-aplikace-na-nette-frameworku) by [Filip Procházka](https://github.com/hosiplan) @ [Mishak's extension](https://github.com/mishak87/newrelic-nette).
+
+Requirements
+------------
+
+blueweb/newrelic-nette requires PHP 5.3.2 or higher.
+
+- [Nette Framework @dev](https://github.com/nette/nette)
+
+
+Installation
+------------
+
+The best way to install blueweb/NewRelic-Nette is using  [Composer](http://getcomposer.org/):
+
+```sh
+$ composer require blueweb/newrelic-nette:@dev
+```
+
+
+-----
+
+Repository [https://github.com/blueweb/newrelic-nette](https://github.com/blueweb/newrelic-nette).
---
+++ src/Blueweb/NewRelicLogger/NewRelicProfilingListener.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Blueweb\NewRelicLogger;
+
+use Kdyby;
+use Nette;
+use Nette\Application\Application;
+use Nette\Application\Request;
+use Tracy\Debugger;
+use Tracy\Logger;
+
+class NewRelicProfilingListener extends Nette\Object implements Kdyby\Events\Subscriber
+{
+
+	public function getSubscribedEvents()
+	{
+		return array(
+			'Nette\\Application\\Application::onStartup',
+			'Nette\\Application\\Application::onRequest',
+			'Nette\\Application\\Application::onError',
+		);
+	}
+
+
+	public function onStartup(Application $app)
+	{
+		if (!extension_loaded('newrelic')) {
+			return;
+		}
+
+		$oldLogger = Debugger::getLogger();
+
+		$logger = new Logger($oldLogger->directory, $oldLogger->email);
+		$logger->emailSnooze = $oldLogger->emailSnooze;
+		$logger->mailer = $oldLogger->mailer;
+
+		Debugger::setLogger($logger);
+	}
+
+
+	public function onRequest(Application $app, Request $request)
+	{
+		if (!extension_loaded('newrelic')) {
+			return;
+		}
+
+		if (PHP_SAPI === 'cli') {
+			newrelic_name_transaction('$ ' . basename($_SERVER['argv'][0]) . ' ' . implode(' ', array_slice($_SERVER['argv'], 1)));
+
+			newrelic_background_job(TRUE);
+
+			return;
+		}
+
+		$params = $request->getParameters();
+		newrelic_name_transaction($request->getPresenterName() . (isset($params['action']) ? ':' . $params['action'] : ''));
+	}
+
+
+	public function onError(Application $app, \Exception $e)
+	{
+		if (!extension_loaded('newrelic')) {
+			return;
+		}
+
+		if ($e instanceof Nette\Application\BadRequestException) {
+			return;
+		}
+
+		newrelic_notice_error($e->getMessage(), $e);
+	}
+
+}
